c11686db94fddeb4e29812a96ba6b149112bd8ee
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-08-03 19:18:29 +0500

    initial commit

---
+++ defining-tasks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Defining small tasks
+
+echo
+whoami
+echo
+pwd
+echo
+hostname
+echo
+ls -ltr
+echo
---
+++ defining-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Example of defining variables
+
+a=Sufiyaan
+b=Usmani
+c='Shell Scripting'
+
+echo "My name is $a $b"
+echo "This is $c"
